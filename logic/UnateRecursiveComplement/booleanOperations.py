59c0644961a69cfb69f5692b9fccc59f352bcf3c
Osakpolor Obaseki <obasekiosa@gmail.com>
2019-07-17 09:45:10 +0100

    Implement copyCube function

--- logic/UnateRecursiveComplement/booleanOperations.py
+++ logic/UnateRecursiveComplement/booleanOperations.py
@@ -1,3 +1,19 @@
+def copyCube(cube):
+    copy = []
+    for e in cube:
+        if e == (0, 1):
+            copy.append((0, 1))
+        elif e == (1, 0):
+            copy.append((1, 0))
+        elif e == (1, 1):
+            copy.append((1, 1))
+        elif e == (0, 0):
+            copy.append((0, 0))
+        else:
+            print("Invalid entry")
+    
+    return copy
+
 def OR(F, G):
     F_or_G = []
     F_or_G.append(F)
